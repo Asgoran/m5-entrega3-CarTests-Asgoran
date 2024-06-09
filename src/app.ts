64eed6c0b8f08884d3dbfc43288f98bbd5d6dfda
Felipe Santos <asgoransantos@gmail.com>
2024-06-09 18:14:29 -0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+// import "express-async-errors";
+// import helmet from "helmet";
+// import { handleErrors } from "./middlewares";
+
+export const app = express();
+
+// app.use(helmet());
+
+// app.use(express.json());
+
+
+// app.use('/categories', categoryRouter);
+
+// app.use(handleErrors);
